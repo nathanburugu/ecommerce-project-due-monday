f3d9a642008962ecaf2c9c5451c26c27ca397733
ybnate254 <95563035+ybnate254@users.noreply.github.com>
2022-10-28 07:48:32 +0300

    online shop coding with basir

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
