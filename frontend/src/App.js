3ba44772781d7a42596bdceb5399691bc9182860
ybnate254 <95563035+ybnate254@users.noreply.github.com>
2022-10-28 12:42:27 +0300

    createdto components in the screen folser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 /** @format */
 
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazona</a>
-      </header>
-      <main>List Products</main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+/** @format */
+
+//creating an object to return sample products
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStck: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high Quality Shirt',
+    },
+    {
+      name: 'Nike Slim Pants',
+      slug: 'nike-slim-pants',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStck: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high Quality Shirt',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStck: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high Quality Shirt',
+    },
+    {
+      name: 'Nike Fit Pant',
+      slug: 'nike-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 120,
+      countInStck: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high Quality Shirt',
+    },
+  ],
+};
+export default data;
